1eafc0ba506f88929eb80fd2c376a74905860e49
Franco Becvort <franbecvort@gmail.com>
2025-03-18 23:02:49 +0000

    structure

--- sidebars.ts
+++ sidebars.ts
@@ -24,7 +24,7 @@ const sidebars: SidebarsConfig = {
     {
       type: 'category',
       label: 'Tutorial',
-      items: ['tutorial-basics/create-a-document'],
+      items: ['spring-boot-in-a-nutshell/create-a-document'],
     },
   ],
    */
