9342ce1e4f086daceeaf198963740c50275489b9
Franco Becvort <franbecvort@gmail.com>
2025-03-20 21:11:42 +0000

    updated export default funcion Home()

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Everything I wish someone has told me when I got into my first Spring Boot project">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
